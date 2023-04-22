aeecdbc76b8b9d78d094bc1c126fa481110496d0
Jake Nead <jakenead@gmail.com>
2023-04-22 16:39:26 -0700

    Rough outline of features/objectives

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import test from './test.js';
+
+console.log('Hello');
+test();
+
+// project factory function that connects with tasks
+
+// task factory function
+
+// checkbox
+// task name
+// notes
+// due Date
+// priority
+// edit button
+// delete button
