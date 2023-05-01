a4a6e9b8824610e8048ae271b8e7ea6b0d2350e9
Jake Nead <jakenead@gmail.com>
2023-04-30 20:54:04 -0700

    Include a sample project in the projects variable

--- src/index.js
+++ src/index.js
@@ -7,12 +7,21 @@ import './modal.css';
 
 initPage();
 modalEventListeners();
-
-// let storage = [
+const projects = {
+  'Sample Project': {
+    taskName: 'Submit summer schedule',
+    notes: 'Include vaction dates and doctor appointments.',
+    dueDate: '5/30/23',
+    priority: true,
+    completed: false,
+  },
+};
+// let projects = {
 //     "test project": {
-//         'task 1': {}
+//         taskName: 'laundry'
+//         notes: 'Set size to medium'
 //     }
-// ]
+// }
 
 // search for 'all', 'today', 'next 7 days', 'high priority'
 
