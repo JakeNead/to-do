8d1ce70e48ed6f030fe4261a1ccf95b3960982eb
Jake Nead <jakenead@gmail.com>
2023-05-09 09:46:32 -0700

    Add test content to the projects object

--- src/index.js
+++ src/index.js
@@ -6,18 +6,72 @@ import './normalize.css';
 import './style.css';
 import './modal.css';
 
-initPage();
-modalEventListeners();
+const todo = (function () {
+  const projects = {
+    'Sample Project1': {
+      'Sample Task1': {
+        taskName: 'Submit summer schedule',
+        notes: 'Include vaction dates and doctor appointments.',
+        dueDate: '5/30/23',
+        priority: true,
+        completed: false,
+      },
+      'Sample Task2': {
+        taskName: 'Build Workbench',
+        notes: 'Use Scrapwood from the cabin',
+        dueDate: '8/30/23',
+        priority: false,
+        completed: false,
+      },
+    },
+    'Sample Project2': {
+      'Sample Task': {
+        taskName: 'Submit summer schedule',
+        notes: 'Include vaction dates and doctor appointments.',
+        dueDate: '5/30/23',
+        priority: true,
+        completed: false,
+      },
+    },
+  };
 
-const projects = {
-  'Sample Project': {
-    taskName: 'Submit summer schedule',
-    notes: 'Include vaction dates and doctor appointments.',
-    dueDate: '5/30/23',
-    priority: true,
-    completed: false,
-  },
-};
+  initPage();
+  modalEventListeners();
+
+  // cache dom
+  const projectForm = document.getElementById('project-form');
+  const taskForm = document.getElementById('task-form');
+
+  // bind events
+  projectForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    newProject(projectInput.value).bind(todo);
+    // projects[projectInput.value] = {};
+    console.log(projects);
+  });
+
+  taskForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    newTask(taskForm.value);
+  });
+
+  return { projects };
+  // submit new project
+  // render the projects nav bar
+  // render the selected project's tasks
+
+//   function isRepeatProject(projectName) {
+//     const nameArray = Object.keys(projects);
+//     for (let i = 0; i < nameArray.length; i++) {
+//       if (nameArray[i].toLowerCase() === projectName.toLowerCase()) {
+//         return console.log('That name already exists!');
+//       }
+//     }
+//     return projectName;
+//   }
+}());
+
+console.log(todo.projects);
 
 // search for 'all', 'today', 'next 7 days', 'high priority'
 
@@ -29,3 +83,19 @@ const projects = {
 // edit button
 // delete button
 // completed section
+
+// create a factory function that creates a project and adds tasks as methods.
+
+// new factory function to try out
+
+// const project = function () {
+//   function newTask(project, taskName, notes, dueDate, isPriority, completed = false) {
+//     return {
+//       taskName,
+//       notes,
+//       dueDate,
+//       isPriority,
+//       completed,
+//     };
+//   }
+// };
