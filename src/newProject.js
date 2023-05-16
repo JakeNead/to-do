8e28028f1d309fa605b9e19640142ccbd117107a
Jake Nead <jakenead@gmail.com>
2023-05-15 20:40:56 -0700

    Change nameArray to projectNameArray

--- src/newProject.js
+++ src/newProject.js
@@ -1,7 +1,7 @@
 export default function newProject(projectName, storage) {
-  const nameArray = Object.keys(storage);
-  for (let i = 0; i < nameArray.length; i++) {
-    if (nameArray[i].toLowerCase() === projectName.toLowerCase()) {
+  const projNameArray = Object.keys(storage);
+  for (let i = 0; i < projNameArray.length; i++) {
+    if (projNameArray[i].toLowerCase() === projectName.toLowerCase()) {
       return console.log('That name already exists!');
     }
   }
