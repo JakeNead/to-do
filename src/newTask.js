19185435c371c5306f57d9f01240dbf456af2824
Jake Nead <jakenead@gmail.com>
2023-05-15 20:43:55 -0700

    Deligate  duplicate checking responsibility to a separate function

--- src/newTask.js
+++ src/newTask.js
@@ -7,39 +7,14 @@ export default function newTask(
   isPriority,
   completed = false,
 ) {
-  // for (const prop)
-  // if (projects[currentProject].toLowerCase() === taskName.toLowerCase()) {
-  //   return console.log('That task name already exists!');
-  // }
+  const taskNameArray = Object.keys(projects[currentProject]);
+  if (taskNameArray.includes(taskName)) {
+    return console.log('That task name already exists!');
+  }
   return {
-    taskName,
     description,
     dueDate,
     isPriority,
     completed,
   };
 }
-// import the function below and run this before adding the task to projects
-
-// function isTaskDuplicate(){
-//     for (const prop in object)
-//   if (projects[currentProject].toLowerCase() === taskName.toLowerCase()) {
-//     return console.log('That task name already exists!');
-//   }
-// }
-
-// set task(setTaskName) {
-//       taskName = setTaskName;
-//     },
-//     set newNotes(setNotes) {
-//       notes = setNotes;
-//     },
-//     set newDueDate(setDueDate) {
-//       dueDate = setDueDate;
-//     },
-//     set newPriority(setPriority) {
-//       isPriority = setPriority;
-//     },
-//     set completed(isCompleted) {
-//       completed = isCompleted;
-//     },
