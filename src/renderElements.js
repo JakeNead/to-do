d860c10b87e303be3cabdc65d0b871a974cda62b
Jake Nead <jakenead@gmail.com>
2023-05-22 21:25:02 -0700

    Add attributes to the task elements

--- src/renderElements.js
+++ src/renderElements.js
@@ -16,12 +16,11 @@ function renderTasks(currPro, taskSection) {
   removeChildElements(taskSection);
   const keys = Object.keys(currPro);
   for (let i = 0; i < keys.length; i += 1) {
-    taskSection.innerHTML += `<div id = '${keys[i]}'>
+    taskSection.innerHTML += `<div class = '${keys[i]}'>
     <span> ${keys[i]} </span>
-    
     <span>${currPro[keys[i]].dueDate}</span>
-    <button>edit</button>
-    <button>delete</button>
+    <button class ='${keys[i]}'>edit</button>
+    <button class ='${keys[i]}'>delete</button>
     <p>${currPro[keys[i]].notes}</p>
     </div>`;
   }
@@ -31,8 +30,10 @@ function editTask(task) {
 // open modal
 // populate modal with object values
 }
-function deleteTask(project, task) {
-  delete project[task];
+function deleteTask(project, task, el) {
+  if (confirm('Are you sure you want to permanently delete this task?')) {
+    // delete project[el.parentElement.getAttribute('class', 1)];
+  }
 }
 
 export {
